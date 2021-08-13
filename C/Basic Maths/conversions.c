0c38abb92e3fcb72ac4bdf32e39d24efb670c054
Rishabh Varshney <right.rishabh@gmail.com>
2021-08-13 21:49:40 +0530

    Update conversions.c

--- C/Basic Maths/conversions.c
+++ C/Basic Maths/conversions.c
@@ -28,3 +28,33 @@ void main() {
 
 //============================================================================
 //============================================================================
+
+
+// program to convert celcius to fahrenheit
+
+void main() {
+    float celcius, fahrenheit;
+    printf("Enter temperature in celcius : ");
+    scanf("%f", &celcius);
+    fahrenheit = (9*celcius + 160)/5;
+    printf("%.2f celcius = %.2f fahrenheit", celcius, fahrenheit);
+}
+
+
+//============================================================================
+//============================================================================
+
+
+// program to convert fahrenheit to celcius
+
+void main() {
+    float celcius, fahrenheit;
+    printf("Enter temperature in fahrenheit : ");
+    scanf("%f", &fahrenheit);
+    celcius = (5*fahrenheit -160)/9;
+    printf("%.2f fahrenheit = %.2f celcius", fahrenheit, celcius);
+}
+
+
+//============================================================================
+//============================================================================
