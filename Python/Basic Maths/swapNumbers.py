243b6f339c829acc61c6120b70b05acf55af737d
Rishabh Varshney <65810355+var-rishabh@users.noreply.github.com>
2021-08-19 21:25:38 +0530

    Create swapNumbers.py

---
+++ Python/Basic Maths/swapNumbers.py
@@ -0,0 +1,11 @@
+# Python program to swap two variables using a temporary variable
+
+x = input('Enter value of x: ')
+y = input('Enter value of y: ')
+
+temp = x
+x = y
+y = temp
+
+print(f'The value of x after swapping: {x}')
+print(f'The value of y after swapping: {y}')
