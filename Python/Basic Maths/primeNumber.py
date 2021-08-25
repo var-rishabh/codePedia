ee860adef1222becf56a9ae7734f77250de4c9c9
Rishabh Varshney <65810355+var-rishabh@users.noreply.github.com>
2021-08-25 21:52:18 +0530

    Create primeNumber.py

---
+++ Python/Basic Maths/primeNumber.py
@@ -0,0 +1,12 @@
+# program to display all the prime numbers within an interval
+
+lower = int(input("Enter lower limit: "))
+upper = int(input("Enter upper limit: "))
+print("Prime numbers between", lower, "and", upper, "are:")
+for num in range(lower, upper + 1):
+   if num > 1:
+       for i in range(2, num):
+           if (num % i) == 0:
+               break
+       else:
+           print(num)
