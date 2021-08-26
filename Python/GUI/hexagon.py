2d4f80a9847351d060296b1bf6a673aa6c2f08c2
Rishabh Varshney <right.rishabh@gmail.com>
2021-08-26 20:41:08 +0530

    more gui added

--- Python/GUI/hexagon.py
+++ Python/GUI/hexagon.py
@@ -7,7 +7,7 @@ col = ("red", "yellow", "cyan", "green", "pink", "white")
 t = turtle.Turtle()
 screen = turtle.Screen()
 screen.bgcolor("black")
-t.speed(25)
+t.speed(15)
 
 for i in range(150):
     t.color(col[i%6])
