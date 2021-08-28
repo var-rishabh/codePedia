5241d49d36fcae736062ee62571c1fcb8959e2ea
Rishabh Varshney <right.rishabh@gmail.com>
2021-08-28 21:04:46 +0530

    Update addMatrix.py

--- Python/Data Types/List/addMatrix.py
+++ Python/Data Types/List/addMatrix.py
@@ -30,3 +30,34 @@ print("Sum is:")
 
 for r in result:
    print(r)
+   
+   
+#============================================================================
+#============================================================================
+
+
+# program to add two matrices using list comprehension
+
+X = [[0,0,0],
+         [0,0,0],
+         [0,0,0]]
+Y = [[0,0,0],
+         [0,0,0],
+         [0,0,0]]
+
+for i in range(3):
+    for j in range(3):
+        x = int(input(f"Enter element X[{i+1}][{j+1}]: "))
+        X[i][j] = x
+print("\n")
+for i in range(3):
+    for j in range(3):
+        x = int(input(f"Enter element Y[{i+1}][{j+1}]: "))
+        Y[i][j] = x
+print("\n")
+
+result = [[X[i][j] + Y[i][j]  for j in range(len(X[0]))] for i in range(len(X))]
+
+print("Sum is:")
+for r in result:
+   print(r)
