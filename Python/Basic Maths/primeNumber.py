fb419bbd9b717cdb23ea0736f33665094076d4f1
Rishabh Varshney <right.rishabh@gmail.com>
2021-10-05 18:16:55 +0530

    more added

--- Python/Basic Maths/primeNumber.py
+++ Python/Basic Maths/primeNumber.py
@@ -1,12 +1,30 @@
-# program to display all the prime numbers within an interval
+# program to print prime numbers
+
+num = int(input("Enter a number: "))
+for x in range(1, num + 1):
+    for i in range(2, x):
+        if (x % i) == 0:
+            break
+    else:
+        print(x)
+
+
+#============================================================================
+#============================================================================
+
+
+# program to print all the prime numbers within an interval
 
 lower = int(input("Enter lower limit: "))
 upper = int(input("Enter upper limit: "))
 print("Prime numbers between", lower, "and", upper, "are:")
 for num in range(lower, upper + 1):
-   if num > 1:
-       for i in range(2, num):
-           if (num % i) == 0:
-               break
-       else:
-           print(num)
+    for i in range(2, num):
+        if (num % i) == 0:
+            break
+    else:
+        print(num)
+
+
+#============================================================================
+#============================================================================
