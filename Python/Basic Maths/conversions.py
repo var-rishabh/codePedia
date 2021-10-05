147be0c3db5fc9a57aa6dd86bb75576f749d5511
Rishabh Varshney <right.rishabh@gmail.com>
2021-10-05 16:59:34 +0530

    added more

--- Python/Basic Maths/conversions.py
+++ Python/Basic Maths/conversions.py
@@ -18,3 +18,24 @@ print(f"{m} m = {km} km")
 
 #============================================================================
 #============================================================================
+
+
+# program to convert celcius to fahrenheit
+
+celcius = float(input("Enter temperature in celcius : "))
+fahrenheit = (9*celcius + 160)/5
+print(f"{celcius} celcius = {fahrenheit} fahrenheit")
+
+
+#============================================================================
+#============================================================================
+
+# program to convert fahrenheit to celcius
+
+fahrenheit = float(input("Enter temperature in celcius : "))
+celcius = (5*fahrenheit -160)/9
+print(f"{fahrenheit} fahrenheit = {celcius} celcius")
+
+
+#============================================================================
+#============================================================================
