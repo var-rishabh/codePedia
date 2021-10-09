b3c99c1ec677d352c17573cc0efb074c69691529
Rishabh Varshney <right.rishabh@gmail.com>
2021-10-09 19:32:54 +0530

    Update basicOperations.cpp

--- C++/Basic Maths/basicOperations.cpp
+++ C++/Basic Maths/basicOperations.cpp
@@ -18,6 +18,23 @@ int main()
 //============================================================================
 
 
+// program to add n number of integers
+
+int main() {
+    int num, sum = 0, i, n;
+    cout << "How many number do you want to add? : ";
+    cin >> n;
+    for (i = 1; i <= n; i++) {
+        cout << "Enter number " << i << ": ";
+        cin >> num;
+        sum = sum + num;
+    }
+    cout << "Sum is : " << sum;
+    return 0;
+}
+
+//============================================================================
+//============================================================================
 // program to subtract two numbers
 
 int main()
