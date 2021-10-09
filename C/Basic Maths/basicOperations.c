45d5c511a06a4d40dea28c229ae53c39c113c054
Rishabh Varshney <right.rishabh@gmail.com>
2021-10-09 19:31:47 +0530

    Update basicOperations.c

--- C/Basic Maths/basicOperations.c
+++ C/Basic Maths/basicOperations.c
@@ -15,6 +15,25 @@ void main() {
 //============================================================================
 
 
+// program to add n number of integers
+
+void main() {
+    int num, sum=0, i, n;
+    printf("How many number do you want to add? : ");
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++) {
+        printf("Enter number %d: ", i);
+        scanf("%d", &num);
+        sum = sum + num;
+    }
+    printf("Sum is: %d", sum);
+}
+
+
+//============================================================================
+//============================================================================
+
+
 // program to subtract two numbers
 
 void main() {
