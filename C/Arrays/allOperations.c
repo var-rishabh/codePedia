19287156bcda5f1aa88085ca145f22dd8ed511fb
Rishabh Varshney <right.rishabh@gmail.com>
2021-10-14 19:14:27 +0530

    Update allOperations.c

--- C/Arrays/allOperations.c
+++ C/Arrays/allOperations.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 struct Array
 {
@@ -128,54 +129,55 @@ int Average(struct Array arr) {                                 /* -- Average of
     return sum/arr.length;
 }
 
-// void ReverseType1(struct Array *arr) {                          /* -- Reversing an array by using new array */
-//     int i, j, k;
-//     int *B = new int[arr->length];
-//     for (i=arr->length-1, j=0; i>=0; i--, j++) {
-//         B[j] = arr->A[i];
-//     }
-//     for (k = 0; k < arr->length; k++) {
-//         arr->A[k] = B[k];
-//     }
-// }
+void ReverseType1(struct Array *arr) {                          /* -- Reversing an array by using new array */
+    int i, j, k;
+    int *B;
+    B = (int *)malloc(arr->length*sizeof(int));
+    for (i=arr->length-1, j=0; i>=0; i--, j++) {
+        B[j] = arr->A[i];
+    }
+    for (k = 0; k < arr->length; k++) {
+        arr->A[k] = B[k];
+    }
+}
 
-// void ReverseType2(struct Array *arr) {                          /* -- Reversing an array by swapping elements */
-//     int i, j;
-//     for (i=0, j=arr->length-1; i<j; i++, j--) {
-//         Swap(&arr->A[i], &arr->A[j]);
-//     }
-// }
+void ReverseType2(struct Array *arr) {                          /* -- Reversing an array by swapping elements */
+    int i, j;
+    for (i=0, j=arr->length-1; i<j; i++, j--) {
+        Swap(&arr->A[i], &arr->A[j]);
+    }
+}
 
-// void InsertSort(struct Array *arr, int insert) {                /* -- Inserting element in a sorted array */
-//     int i = arr->length - 1;
-//     if (arr->length == arr->size) {
-//         return;
-//     }
-//     while (i>=0 && arr->A[i]>insert) {
-//         arr->A[i+1] = arr->A[i];
-//         i--;
-//     }
-//     arr->A[i+1] = insert;
-//     arr->length++;
-// }
+void InsertSort(struct Array *arr, int insert) {                /* -- Inserting element in a sorted array */
+    int i = arr->length - 1;
+    if (arr->length == arr->size) {
+        return;
+    }
+    while (i>=0 && arr->A[i]>insert) {
+        arr->A[i+1] = arr->A[i];
+        i--;
+    }
+    arr->A[i+1] = insert;
+    arr->length++;
+}
 
-// int IsSorted(struct Array arr) {                               /* -- Checking if array is sorted or not */
-//     for (int i = 0; i < arr.length - 1; i++) {
-//         if (arr.A[i] > arr.A[i+1])
-//             return 0;
-//     }
-//     return 1;
-// }
+int IsSorted(struct Array arr) {                               /* -- Checking if array is sorted or not */
+    for (int i = 0; i < arr.length - 1; i++) {
+        if (arr.A[i] > arr.A[i+1])
+            return 0;
+    }
+    return 1;
+}
 
-// void Rearrange(struct Array *arr) {
-//     int i = 0;
-//     int j = arr->length - 1;
-//     while (i < j) {
-//         while (arr->A[i] < 0) i++;
-//         while (arr->A[j] > 0) j--;
-//         if (i < j) Swap(&arr->A[i], &arr->A[j]);
-//     }
-// }
+void Rearrange(struct Array *arr) {
+    int i = 0;
+    int j = arr->length - 1;
+    while (i < j) {
+        while (arr->A[i] < 0) i++;
+        while (arr->A[j] > 0) j--;
+        if (i < j) Swap(&arr->A[i], &arr->A[j]);
+    }
+}
 
 // struct Array* Merge(struct Array *arr1, struct Array *arr2) {            /* -- Merging two array */
 //     int i, j, k;
