e5043da60222367b748ee36bd5f4dbcdda0fa563
Rishabh Varshney <right.rishabh@gmail.com>
2021-11-08 13:36:41 +0530

    Create basicOperations.java

---
+++ Java/Basic Maths/basicOperations.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class sum {
+    public static void main(String[] args) {
+        int x, y, sum;
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Enter number 1:");
+        x = myObj.nextInt();
+    
+        System.out.println("Enter number 2:");
+        y = myObj.nextInt();
+    
+        sum = x + y;  // Calculate the sum of x + y
+        System.out.println("Sum is: " + sum);
+    }
+}
+
+
+//============================================================================
+//============================================================================
