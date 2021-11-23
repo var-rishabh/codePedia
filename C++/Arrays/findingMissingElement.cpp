4e6fc2aef5965ef1ed4dfb5fd2a6658962be80d1
Rishabh Varshney <right.rishabh@gmail.com>
2021-11-23 14:36:41 +0530

    Update findingMissingElement.cpp

--- C++/Arrays/findingMissingElement.cpp
+++ C++/Arrays/findingMissingElement.cpp
@@ -14,11 +14,6 @@ int Missing1(int A[], int size) {          // finding missing element
     return 0;
 }
 
-// int Missing2(int A[], int size) {          // finding missing element using hash mapping
-//     int newA[size];
-
-// }
-
 void MulMissing(int A[], int size) {          // finding multiple missing element
     int diff = A[0] - 0;
     for (int i = 0; i < size; i++) {
@@ -33,17 +28,7 @@ void MulMissing(int A[], int size) {          // finding multiple missing elemen
 
 
 int main() {
-    int A[10] = {1, 2, 4, 5, 6, 7, 8, 11, 12, 15};
-    // Missing2(A, 10);
-
-    int n[15];
-    int l = 1;
-    int h = 15;
-    
-    for (int i = 0; i < 10; i++) {
-        n[A[i]]++;
-    }
-    for (int j = l; j <= h; j++) {
-        if (n[j] == 0) cout << j << endl;
-    }
+    int A[10] = {1, 2, 4, 5, 6, 7, 8, 9, 10, 11};
+    cout << Missing1(A, 10);
+    return 0;
 }
