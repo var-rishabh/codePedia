d37c53625845281e2bd3dcabf7e465d15d235d20
Rishabh Varshney <right.rishabh@gmail.com>
2021-11-23 23:54:17 +0530

    Update findMissingElement.c

--- C/Arrays/findMissingElement.c
+++ C/Arrays/findMissingElement.c
@@ -27,8 +27,43 @@ void MulMissing(int A[], int size) {          // finding multiple missing elemen
 
 
 int main() {
-    int A[10] = {1, 2, 4, 5, 6, 7, 8, 9, 12, 13};
+    // int A[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 12};
+    int A[10] = {1, 3, 8, -10, 16, 7, 5, 2, 9, 132};
     // printf("%d \n", Missing1(A, 10));
     // MulMissing(A, 10);
+
+    // for (int i = 0; i < 10; i++) {          // finding pair of elements
+    //     for (int j = i+1; j < 10; j++) {
+    //         if (A[i] + A[j] == 11) {
+    //             printf("%d %d \n", A[i], A[j]);
+    //         }
+    //     }
+    // }
+
+    // int i = 0, j = 9;           // finding pair in sorted array
+    // int n = 13;
+    // while (i < j) {
+    //     if (A[i] + A[j] == n) {
+    //         printf("%d %d \n", A[i], A[j]);
+    //         i++;
+    //         j--;
+    //     } else if (A[i] + A[j] < n) {
+    //         i++;
+    //     } else {
+    //         j--;
+    //     }        
+    // }
+
+
+    // find max and min in one scan
+
+    int max = A[0];
+    int min = A[0];
+    for (int k = 0; k < 10; k++) {
+        if (min > A[k]) min = A[k];
+        else if (max < A[k]) max = A[k]; 
+    }
+    printf("%d %d", max, min);
+
     return 0;
 }
