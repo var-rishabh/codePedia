f58f623e0f5db37b562c652b0fd9e6e842203c32
Rishabh Varshney <right.rishabh@gmail.com>
2021-12-23 11:11:19 +0530

    updated

--- C++/Arrays/findDuplicates.cpp
+++ C++/Arrays/findDuplicates.cpp
@@ -4,19 +4,9 @@ using namespace std;
 
 // program to find the duplicates in a sorted array
 
-void Duplicate1(int A[]) {
-    int lD = 0;
-    for (int i = 0; i < 10; i++) {
-        if (A[i] == A[i+1] && A[i] != lD) {
-            cout << A[i] << " ";
-            lD = A[i];
-        }
-    }
-}
-
-void Duplicate2(int A[]) {
+void Duplicate1(int A[], int size) {
     int j;
-    for (int i = 0; i < 9 ; i++) {
+    for (int i = 0; i < size ; i++) {
         if (A[i] == A[i+1] ) {
             j = i + 1;
             while (A[j] == A[i]) j++;
@@ -29,26 +19,28 @@ void Duplicate2(int A[]) {
 
 // program to find the duplicates in a unsorted array
 
-void Duplicate3(int A[]) {
-    for (int i = 0; i < 9; i++) {
-        int count = 1;
-        if (A[i] != -1) {
-            for (int j = i+1; i < 10; j++) {
-                if (A[i] == A[j]) {
-                    count++;
-                    A[j] = -1;
-                }
-            }
-            if (count > 1) {
-                cout << A[i] << endl; 
-                cout << count << endl; 
-            }
-        }
-    }
-}
+// void Duplicate2(int A[], int size) {
+//     for (int i = 0; i < size; i++) {
+//         int count = 1;
+//         if (A[i] != -1) {
+//             for (int j = i+1; i < size; j++) {
+//                 if (A[i] == A[j]) {
+//                     count++;
+//                     A[j] = -1;
+//                 }
+//             }
+//             if (count > 1) {
+//                 cout << A[i] << " - " << count << endl; 
+//             }
+//         }
+//     }
+// }
 
 int main() {
-    int A[10] = {22, 11, 33, 22, 44, 66, 66, 55, 66, 22};
-    Duplicate2(A);
+    int A[10] = {11, 22, 33, 33, 44, 55, 66, 66, 66, 66};
+    Duplicate1(A, 10);
+    
+    // int A[10] = {22, 11, 33, 22, 44, 66, 66, 55, 66, 22};
+    // Duplicate2(A, 10);
     return 0;
 }
