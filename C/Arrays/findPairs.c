813ea8a2f2950bd95bdddf895ddd75ab61460346
Rishabh Varshney <right.rishabh@gmail.com>
2021-12-23 10:45:07 +0530

    updated

--- C/Arrays/findPairs.c
+++ C/Arrays/findPairs.c
@@ -28,10 +28,31 @@ void MulMissing(int A[], int size) {          // finding multiple missing elemen
 
 int main() {
     int A[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 12};
+    int A[10] = {1, 3, 8, -10, 16, 7, 5, 2, 9, 132};
+    // printf("%d \n", Missing1(A, 10));
+    // MulMissing(A, 10);
 
-    printf("%d \n", Missing1(A, 10));
-    
-    MulMissing(A, 10);
+    for (int i = 0; i < 10; i++) {          // finding pair of elements
+        for (int j = i+1; j < 10; j++) {
+            if (A[i] + A[j] == 11) {
+                printf("%d %d \n", A[i], A[j]);
+            }
+        }
+    }
+
+    int i = 0, j = 9;           // finding pair in sorted array
+    int n = 13;
+    while (i < j) {
+        if (A[i] + A[j] == n) {
+            printf("%d %d \n", A[i], A[j]);
+            i++;
+            j--;
+        } else if (A[i] + A[j] < n) {
+            i++;
+        } else {
+            j--;
+        }        
+    }
 
     return 0;
 }
