1278588f82861d96684549e012369d8442c5268e
Rishabh Varshney <65810355+var-rishabh@users.noreply.github.com>
2022-07-13 01:22:23 +0530

    Difficulty - Easy | Score - 10.00 💚

--- HackerRank/Python/Designer-Door-Mat/DesignerDoorMat.py
+++ HackerRank/Python/Designer-Door-Mat/DesignerDoorMat.py
@@ -26,9 +26,9 @@ for x in range(1, n//2 + 1):
 
 # Solution 2
 
-x,y = map(int,input().split())
+x, y = map(int,input().split())
 items = list(range(1,x+1,2))
-items = items+items[::-1][1:]
+items = items + items[::-1][1:]
 for i in items:
     text= "WELCOME" if i == x else '.|.'*i
     print (text.center(y,'-'))
