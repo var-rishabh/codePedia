d6f37e3748b747a7314f5c35a53a371369bf16c3
Rishabh Varshney <65810355+var-rishabh@users.noreply.github.com>
2022-07-18 22:22:56 +0530

    Difficulty - Easy | Score - 20.00 💚

---
+++ HackerRank/Python/25-Capitalize/Capitalize.py
@@ -0,0 +1,18 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the solve function below.
+def solve(s):
+    for i in s.split():
+        s = s.replace(i,i.capitalize())
+    return s
+
+if __name__ == '__main__':
+    s = input()
+    result = solve(s)
+    print(result)
