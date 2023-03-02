44151acd5083996b6fec25a935e3792debfba3e7
Rishabh Varshney <right.rishabh@gmail.com>
2023-03-02 20:07:22 +0530

    Time: 3 ms (57.8%), Space: 5.9 MB (92.74%)

---
+++ Leetcode/07-Reverse-Integer/Reverse-Integer.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int rev = 0;
+        while (x != 0) {
+            if (rev > INT_MAX / 10 || rev < INT_MIN / 10) {
+                return 0;
+            }
+            rev = rev * 10 + x % 10;
+            x = x / 10;
+        }
+        return rev;
+    }
+}; 
