22bb4a399f95146bc48d525c90f5d52a647c3120
Rishabh Varshney <right.rishabh@gmail.com>
2023-08-18 17:07:39 +0530

    sorting completed

--- cpp-basics/sorting.cpp
+++ cpp-basics/sorting.cpp
@@ -52,17 +52,68 @@ vector<int> insertionSort(vector<int> arr, int n) {
     return arr;
 }
 
-vector<int> mergeSort(vector<int> arr, int n) {
-    // worst and average time complexity = O(n^2)
+int partition(vector<int> &arr, int low, int high) {
+    int pivot = arr[low];
+    int l = low;
+    int r = high;
+    while (l < r) {
+        while (arr[l] <= pivot && l <= high - 1) {
+            l++;
+        }
+        while (arr[r] > pivot && r >= low + 1) {
+            r--;
+        }
+        if (l < r) {
+            swap(arr[l], arr[r]);
+        }
+    }
+    swap(arr[low], arr[r]);
+    return r;
+}
+
+void quickSort(vector<int> &arr, int low, int high) {
+    // worst and average time complexity = O(nlogn)
     // best time complexity = O(n)
-    // for (int i = 1; i < n; i++) {
-    //     int j = i;
-    //     while (j > 0 && arr[j - 1] > arr[j]) {
-    //         swap(arr[j - 1], arr[j]);
-    //         j--;
-    //     }
-    // }
-    // return arr;
+    if (low < high) {
+        int part = partition(arr, low, high);
+        quickSort(arr, low, part - 1);
+        quickSort(arr, part + 1, high);
+    }
+}
+
+void merge(vector<int> &arr, int l, int mid, int r) {
+    int left = l;
+    int right = mid + 1;
+    vector<int> temp;
+    while (left <= mid && right <= r) {
+        if (arr[left] < arr[right]) {
+            temp.push_back(arr[left]);
+            ++left;
+        } else {
+            temp.push_back(arr[right]);
+            ++right;
+        }
+    }
+    while (left <= mid) {
+        temp.push_back(arr[left]);
+        ++left;
+    }
+    while (right <= r) {
+        temp.push_back(arr[right]);
+        ++right;
+    }
+    for (int i = l; i <= r; i++) {
+        arr[i] = temp[i - l];
+    }
+}
+
+void mergeSort(vector<int> &arr, int l, int r) {
+    // best, worst and average time complexity = O(nlogn)
+    if (l == r) return;
+    int mid = (l + r) / 2;
+    mergeSort(arr, l, mid);
+    mergeSort(arr, mid + 1, r);
+    merge(arr, l, mid, r);
 }
 
 int main() {
@@ -76,10 +127,11 @@ int main() {
     // vector<int> result = selectionSort(arr, 10);
     // vector<int> result = bubbleSort(arr, 10);
     // vector<int> result = insertionSort(arr, 10);
-    vector<int> result = mergeSort(arr, 10);
+    quickSort(arr, 0, 9);
+    // mergeSort(arr, 0, 9);
 
     cout << "\nNew Array: " << endl;
-    for (auto i : result) {
+    for (auto i : arr) {
         cout << i << " ";
     }
     return 0;
