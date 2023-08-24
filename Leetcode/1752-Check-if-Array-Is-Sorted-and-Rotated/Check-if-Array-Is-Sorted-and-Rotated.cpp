b01a72467510b845bcdd70320d3e37bd92099fdf
Rishabh Varshney <right.rishabh@gmail.com>
2023-08-24 21:25:03 +0530

    Time: 2 ms (46.31%), Space: 8.4 MB (77.20%)

---
+++ Leetcode/1752-Check-if-Array-Is-Sorted-and-Rotated/Check-if-Array-Is-Sorted-and-Rotated.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool check(vector<int> &nums) {
+        int n = nums.size();
+        int cnt = 0;
+        for (int i = 1; i < n; i++) {
+            if (nums[i - 1] > nums[i]) {
+                cnt++;
+            }
+        }
+        if (nums[n - 1] > nums[0]) {
+            cnt++;
+        }
+        if (cnt <= 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+};
